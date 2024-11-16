36e5871171105204848733060effb8637efce64e
erand44 <99759915+erand44@users.noreply.github.com>
2024-11-16 21:46:37 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,2 @@
 
-let firstName = "Erand";
-console.log(firstName);
-let lastName = "Sadiku";
-console.log(lastName);
-//GLÖM ALDRIG DENNA SYMBOLEN I SLUTET --------> 
+//här stog någon consle.log om mitt  namnohc efternamn....
